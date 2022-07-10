2b55d2313a214cad051bb36e2295c4a4113cd4e3
ksvaughan <sk65594@mail.harpercollege.edu>
2022-07-10 10:44:01 -0500

    updated gallery

--- js/geo.js
+++ js/geo.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
-    lightbox.option ({
-      'resizeDuration': 200,
-      'wrapAround': true,
-      'albumLabel': 
+  lightbox.option({
+    albumLabel: 'Image %1 of %2',
+    alwaysShowNavOnTouchDevices: false,
+    fadeDuration: 600,
+    fitImagesInViewport: true,
+    imageFadeDuration: 600,
+    maxWidth: 800,
+    maxHeight: 600,
+    positionFromTop: 50,
+    resizeDuration: 700,
+    showImageNumberLabel: true,
+    wrapAround: false, 
+    disable<a href="https://www.jqueryscript.net/tags.php?/Scroll/">Scroll</a>ing:false,
      
     )}
 });
