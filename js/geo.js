1c7d87b1e9c0e8b06abd987c04f9b17306dfae1b
ksvaughan <sk65594@mail.harpercollege.edu>
2022-09-18 14:41:12 -0500

    updated GIS and Gallery pages

--- js/geo.js
+++ js/geo.js
@@ -1,3 +1,44 @@
+
+
+    $(document).ready(function () {
+
+      $('.slideshow').slideshowPlugin({
+        // Parameters
+        effect: 'sliding',
+        slideSpeed: 800,
+        ratio: "keep",
+      });
+
+      // $('.slideshow-2').slideshowPlugin({
+      //   effect: 'fade',
+      //   ratio: "keep",
+      // });
+
+      // $('.slideshow-3').slideshowPlugin({
+      //   effect: 'photo',
+      //   ratio: "keep",
+      // });
+
+      // $('.slideshow-4').slideshowPlugin({
+      //   effect: 'sliding',
+      //   autoplay: 'true',
+      //   delay: 8000,
+      //   slideSpeed: 700,
+      //   ratio: "keep",
+      // });
+
+
+
+
+
+
+
+
+
+
+
+
+
 // $(document).ready(function () {
 //   lightbox.option({
 //     albumLabel: 'Image %1 of %2',
@@ -15,33 +56,34 @@
      
 //     )}
 // });
-    // w3schools how to js slideshow
+    // w3schools how to js slideshow for one slideshow
     
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+
