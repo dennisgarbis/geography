02d6a9e2d6b6adeb80ca541e29e1ad28fe626f2c
ksvaughan <sk65594@mail.harpercollege.edu>
2022-10-17 07:53:04 -0500

    updated gallery w/ bootstrap. faculty page updates

---
+++ js/gegsite.js
@@ -0,0 +1,40 @@
+// references:  https://www.w3schools.com/bootstrap/bootstrap_ref_js_carousel.asp
+// https://getbootstrap.com/docs/5.2/components/carousel/
+
+
+// CAROUSEL
+
+const carousel = new bootstrap.Carousel('#myCarousel');
+const carousel2 = new bootstrap.Carousel('#myCarousel2');
+
+// MODALS
+
+// https://getbootstrap.com/docs/5.2/components/modal/
+
+const exampleModal = document.getElementById('exampleModal');
+const exampleModal2 = document.getElementById('exampleModal2');
+const exampleModal3 = document.getElementById('exampleModal3');
+const exampleModal4 = document.getElementById('exampleModal4');
+const exampleModal5 = document.getElementById('exampleModal5');
+const exampleModal6 = document.getElementById('exampleModal6');
+const exampleModal7 = document.getElementById('exampleModal7');
+const exampleModal8 = document.getElementById('exampleModal8');
+const exampleModal9 = document.getElementById('exampleModal9');
+
+
+
+
+
+
+
+
+
+
+// You can create a carousel instance with the carousel constructor,
+// for example, to initialize with additional options and start cycling through items:
+
+// const myCarouselElement = document.querySelector('#myCarousel')
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   interval: 2000,
+//   wrap: false
+// })
